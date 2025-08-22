5f15297af1f19370f180be04adf31c3b7fe9d5a7
hildanku <hildankutomo@gmail.com>
2025-08-22 16:15:11 +0700

    chore(career): move career routes to folder career

--- src/routes/career/index.tsx
+++ src/routes/career/index.tsx
@@ -1,6 +1,6 @@
 import CareerGalery from '@/components/pages/career'
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/career')({
+export const Route = createFileRoute('/career/')({
     component: CareerGalery,
 })
