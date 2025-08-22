7ca30e10580b0d629877e76ea0f0d19f371dd092
hildanku <hildankutomo@gmail.com>
2025-08-22 15:27:00 +0700

    feat: add base-repository to make each repository is consistency

---
+++ src/lib/repository/base-repository.ts
@@ -0,0 +1,43 @@
+export type OrderQuery = 'ASC' | 'DESC'
+
+export type ListArgs = {
+	sort: string
+	order: OrderQuery
+	q: string
+	limit: number
+	page: number
+	[key: string]: any
+}
+
+export type CreateArgs<T> = {
+	item: Omit<T, 'id' | 'created_at' | 'updated_at'>
+}
+
+export type FindByIdArgs = {
+	id: number
+}
+
+export type UpdateArgs<T> = {
+	id: number
+	item: Partial<Omit<T, 'id'>>
+}
+
+export type DeleteArgs = {
+	id: number
+}
+
+export interface AsyncBaseRepository<T> {
+	create(args: CreateArgs<T | Record<string, any>>): Promise<T[]>
+	findById(args: FindByIdArgs): Promise<T[] | null>
+	list(args: Partial<ListArgs>): Promise<{ items: T[]; meta: { totalItems: number } }>
+	update(args: UpdateArgs<T | Record<string, any>>): Promise<T[] | null>
+	delete(args: DeleteArgs): Promise<boolean>
+}
+
+export interface BaseRepository<T> {
+	create(args: CreateArgs<T | Record<string, any>>): T[]
+	findById(args: FindByIdArgs): T[] | null
+	list(args: Partial<ListArgs>): { items: T[]; meta: { totalItems: number } }
+	update(args: UpdateArgs<T | Record<string, any>>): T[] | null
+	delete(args: DeleteArgs): boolean
+}
