1492b2d613f23b086141bbcd2691a7b81d7e9c4f
hildanku <hildankutomo@gmail.com>
2025-08-25 16:52:15 +0700

    fix(career): make career card in mobile device is two cards per row

--- src/components/pages/career.tsx
+++ src/components/pages/career.tsx
@@ -63,7 +63,7 @@ export default function CareerGalery() {
                             </p>
                         </div>
                     ) : (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                             {careers.map((career) => (
                                 <CareerCard key={career.id} career={career} />
                             ))}
