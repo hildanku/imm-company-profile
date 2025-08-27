0934fc25827efa1b1495a0db2cd0cbb7046c0994
hildanku <hildankutomo@gmail.com>
2025-08-27 11:02:14 +0700

    fix(language): fix some translation #57

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
 import { Clock, Instagram, Linkedin, MapPin, Phone } from "lucide-react"
+import { useLanguage } from '@/hooks/use-language'
 
 export const Footer = () => {
+    const { t } = useLanguage()
     return (
         <section className="bg-black text-white">
             <div className="container mx-auto px-4 py-16">
@@ -8,26 +10,26 @@ export const Footer = () => {
                     <div>
                         <h2 className="text-3xl lg:text-4xl font-bold mb-2">Indonesia Mitra Media</h2>
                         <p className="text-gray-300 text-lg mb-8">
-                            Make your business be #1
+                            {t('footer_tagline')}
                         </p>
                         <div className="space-y-4">
-                            <h3 className="text-xl font-semibold mb-4">Contact Information</h3>
+                            <h3 className="text-xl font-semibold mb-4">{t('contact_information')}</h3>
                             <div className="flex items-center space-x-4">
                                 <Phone className="w-5 h-5 text-gray-400" />
                                 <span>( 0287 ) 551628</span>
                             </div>
                             <div className="flex items-center space-x-4">
                                 <MapPin className="w-5 h-5 text-gray-400" />
-                                <span>Jl. Kepel, Kepel, Candi, Kec. Karanganyar, Kabupaten Kebumen, Jawa Tengah 54364</span>
+                                <span>{t('company_address')}</span>
                             </div>
                             <div className="flex items-center space-x-4">
                                 <Clock className="w-5 h-5 text-gray-400" />
-                                <span>09.00 AM - 05.00 PM</span>
+                                <span>{t('phone_hours')}</span>
                             </div>
                         </div>
                     </div>
                     <div className="flex flex-col space-y-4 lg:mt-27 lg:items-start lg:justify-start">
-                        <h3 className="text-xl font-semibold mb-4">Follow Us</h3>
+                        <h3 className="text-xl font-semibold mb-4">{t('follow_us')}</h3>
                         <div className="flex items-center space-x-4">
                             <a href="https://id.linkedin.com/company/cv-indonesia-mitra-media" target="_blank" rel="noopener noreferrer">
                                 <Linkedin className="w-5 h-5 text-gray-400 hover:text-white transition-colors" />
